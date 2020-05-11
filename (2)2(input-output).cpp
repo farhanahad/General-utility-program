17b333faf0279d31362da15d0f71dca8333cfb2a
Nurul Ahad Farhan <61065246+farhanahad@users.noreply.github.com>
2020-05-12 02:26:07 +0600

    Add files via upload

---
+++ (2)2(input-output).cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    char b;
+    cout<<"Type a number:";
+
+    cin>>a;
+    cout<<"The number is "<<a<<endl;
+    cout<<"Type a character:";
+    cin>>b;
+    cout<<"The character is "<<b;
+}
