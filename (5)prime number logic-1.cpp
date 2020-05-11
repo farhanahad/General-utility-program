fa7804e4e375622f002edf71948d1130ef0eee24
Nurul Ahad Farhan <61065246+farhanahad@users.noreply.github.com>
2020-05-12 02:29:46 +0600

    Add files via upload

---
+++ (5)prime number logic-1.cpp
@@ -0,0 +1,21 @@
+ #include<iostream>
+ using namespace std;
+ int main()
+ {
+     int i,n,count=0;
+     cin>>n;
+     for(i=1;i<=n;i++)
+     {
+         if(n%i==0)
+         {
+             count++;
+         }
+     }
+     if(count==2)
+     {
+         cout<<n<<" is a prime number";
+     }
+     else{
+        cout<<n<<" is not a prime number";
+     }
+ }
