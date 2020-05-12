886c786c88f6f14adba169db00ae066b79394f31
Nurul Ahad Farhan <61065246+farhanahad@users.noreply.github.com>
2020-05-12 16:01:26 +0600

    Add files via upload

---
+++ (16)Area of Equilateral Triangle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    float a;
+    float area;
+    cin>>a;
+    area=sqrt(3)/4*(a*a);
+    cout<<"Area of equilateral triangle:"<<area;
+}
