b0f7628c680aead554b8582a0052ca53b6eb5239
Nurul Ahad Farhan <61065246+farhanahad@users.noreply.github.com>
2020-05-12 15:46:29 +0600

    Add files via upload

---
+++ (13)Area of circle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int radius;
+    float area;
+    cout<<"Enter Radius of the circle:";
+    cin>>radius;
+    area=3.14*radius*radius;
+    cout<<"Area of the circle:"<<area;
+
+}
