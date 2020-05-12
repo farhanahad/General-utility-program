822c3d4741ff596585c5b9004fe620be56ea7e65
Nurul Ahad Farhan <61065246+farhanahad@users.noreply.github.com>
2020-05-12 15:50:02 +0600

    Add files via upload

---
+++ (15)Area of Triangle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int b,h;
+    float area;
+    cin>>b>>h;
+    area=(b*h)/2;
+    cout<<"Area of triangle: "<<area;
+}
