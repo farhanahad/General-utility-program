50190af64005768eab367660beb7c463c347f0a0
Nurul Ahad Farhan <61065246+farhanahad@users.noreply.github.com>
2020-05-12 15:48:13 +0600

    Add files via upload

---
+++ (14)circumstance of circle.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int radius;
+    float cir;
+    cout<<"Enter Radius:";
+    cin>>radius;
+    cir=2*3.1416*radius;
+    cout<<"Circumstance of the circle:"<<cir;
+}
